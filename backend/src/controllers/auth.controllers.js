27638f1bd7f80cfd83ded590e64582cf4fc84ac5
Ayush Das <ayushdas7354@gmail.com>
2026-02-24 12:06:05 +0530

    fixed auth controller

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -29,7 +29,7 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   if(!email || !password){
-    return res.status(400).json({message: "Please Provide"});
+    return res.status(400).json({message: "Incorrect Email or Password"});
   }
 
   try {
